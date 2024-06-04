0e17096f386b49759b65be1301bf8730b6178b9c
KarineAlsa <saremana@hotmail.com>
2024-06-04 01:22:26 -0600

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import './login/login_page.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Login(),
-    debugShowCheckedModeBanner: false
-  ));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Login(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
