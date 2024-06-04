206cbd6ac35046e98bf34e7a18ccbe5a60904a25
andresjuarez1 <213364@ids.upchiapas.edu.mx>
2024-06-04 09:38:26 -0600

    validations

--- lib/createPost/create_post_page.dart
+++ lib/createPost/create_post_page.dart
@@ -11,8 +11,9 @@ class CreatePostPage extends StatefulWidget {
 class _CreatePostPageState extends State<CreatePostPage> {
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _contentController = TextEditingController();
-  File? _image; 
+  File? _image;
   final picker = ImagePicker();
+  final _formKey = GlobalKey<FormState>();
 
   Future getImage() async {
     final pickedFile = await picker.pickImage(source: ImageSource.gallery);
@@ -27,17 +28,20 @@ class _CreatePostPageState extends State<CreatePostPage> {
   }
 
   void _submitPost() {
-    // Enviar la publicación al servidor
-    String title = _titleController.text;
-    String content = _contentController.text;
+    if (_formKey.currentState!.validate()) { 
+      // All fields are valid, proceed with processing
+      String title = _titleController.text;
+      String content = _contentController.text;
 
-    if (_image != null) {
-      print('Ruta de la imagen: ${_image!.path}');
+      if (_image != null) {
+        print('Ruta de la imagen: ${_image!.path}');
+      }
+
+      print('Título: $title');
+      print('Contenido: $content');
+
+      Navigator.pop(context);
     }
-
-    print('Título: $title');
-    print('Contenido: $content');
-    Navigator.pop(context);
   }
 
   @override
@@ -48,41 +52,56 @@ class _CreatePostPageState extends State<CreatePostPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            TextFormField(
-              controller: _titleController,
-              decoration: InputDecoration(
-                labelText: 'Título',
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextFormField(
+                controller: _titleController,
+                decoration: InputDecoration(
+                  labelText: 'Título',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'El título es obligatorio.';
+                  }
+                  return null;
+                },
               ),
-            ),
-            SizedBox(height: 20),
-            TextFormField(
-              controller: _contentController,
-              decoration: InputDecoration(
-                labelText: 'Contenido',
+              SizedBox(height: 20),
+              TextFormField(
+                controller: _contentController,
+                decoration: InputDecoration(
+                  labelText: 'Contenido',
+                ),
+                maxLines: null,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'El contenido es obligatorio.';
+                  }
+                  return null;
+                },
               ),
-              maxLines: null,
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: getImage,
-              child: Text('Seleccionar Imagen'),
-            ),
-            SizedBox(height: 10),
-            _image != null
-                ? Image.file(
-                    _image!,
-                    height: 150,
-                  )
-                : SizedBox(), 
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _submitPost,
-              child: Text('Enviar Publicación'),
-            ),
-          ],
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: getImage,
+                child: Text('Seleccionar Imagen'),
+              ),
+              SizedBox(height: 10),
+              _image != null
+                  ? Image.file(
+                      _image!,
+                      height: 150,
+                    )
+                  : SizedBox(),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _submitPost,
+                child: Text('Enviar Publicación'),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/login/login_page.dart
+++ lib/login/login_page.dart
@@ -2,7 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:c2_movil/register/register_page.dart';
 import 'package:c2_movil/landing/landing.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
   void _navigateToRegistrationPage(BuildContext context) {
     Navigator.push(
       context,
@@ -10,11 +19,13 @@ class Login extends StatelessWidget {
     );
   }
 
-  void _navigateToLandingPage(BuildContext context) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => LandingPage()),
-    );
+  void _navigateToLandingPage() {
+    if (_formKey.currentState!.validate()) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => LandingPage()),
+      );
+    }
   }
 
   @override
@@ -22,38 +33,56 @@ class Login extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(height: 40),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Usuario',
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: 40),
+              TextFormField(
+                controller: _usernameController,
+                decoration: InputDecoration(
+                  labelText: 'Usuario',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'El nombre de usuario es obligatorio.';
+                  }
+                  return null;
+                },
               ),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Contraseña',
+              SizedBox(height: 20),
+              TextFormField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Contraseña',
+                ),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'La contraseña es obligatoria.';
+                  }
+                  return null;
+                },
               ),
-              obscureText: true,
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () => _navigateToLandingPage(context),
-              child: Text('Login'),
-            ),
-            SizedBox(height: 20),
-            GestureDetector(
-              onTap: () => _navigateToRegistrationPage(context),
-              child: Container(
-                margin: EdgeInsets.only(left: 20.0),
-                child: Text(
-                  '¿No estás registrado? Regístrate',
-                  style: TextStyle(fontSize: 14.0, color: Colors.blue),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _navigateToLandingPage,
+                child: Text('Login'),
+              ),
+              SizedBox(height: 20),
+              GestureDetector(
+                onTap: () => _navigateToRegistrationPage(context),
+                child: Container(
+                  margin: EdgeInsets.only(left: 20.0),
+                  child: Text(
+                    '¿No estás registrado? Regístrate',
+                    style: TextStyle(fontSize: 14.0, color: Colors.blue),
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
